cbbb5f4125660c1c1b3c65b6171725d9b568d0fc
SAnABros <53811328+SAnABros@users.noreply.github.com>
2021-02-08 21:56:08 +0200

    1.1
    Now it is smoother)))

--- perlin_noise.py
+++ perlin_noise.py
@@ -4,20 +4,24 @@ from time import time
 import threading
 
 pygame.init()
-WIDTH = 600
-HEIGHT = 600
+WIDTH = 400
+HEIGHT = 400
 sc= pygame.display.set_mode((WIDTH,HEIGHT))
-layer = 20
+layer = 10
 def Layer(scr, k):
-	screen= pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
-	for x in range(0, WIDTH//2**k+1):
-		for y in range(0, HEIGHT//2**k+1):
+	scale = 0.5
+	screen= pygame.Surface((WIDTH*scale, HEIGHT*scale), pygame.SRCALPHA)
+	for x in range(0, int(WIDTH*scale*2/(2**k))+1):
+		for y in range(0, int(HEIGHT*scale*2/(2**k))+1):
 			X = x*(2**k)-(2**(k/2)) if x>0 else 0
 			Y = y*(2**k)-(2**(k/2)) if y>0 else 0
-			W = 2**k if X+2**k<=WIDTH else X-(X+k-WIDTH)
-			H = 2**k if Y+2**k<=HEIGHT else Y-(Y+k-HEIGHT)
-			if rand(0,100)/100>=0.5:
-				pygame.draw.rect(screen, (0,0,0,255/layer+1), [X,Y , W,H])
+			W = 2**k if X+2**k<=WIDTH*scale*2 else X-(X+k-WIDTH*scale*2)
+			H = 2**k if Y+2**k<=HEIGHT*scale*2 else Y-(Y+k-HEIGHT*scale*2)
+			if rand(0,1)/1>=0.5:
+				pygame.draw.rect(screen, (0,0,0), [X,Y , W,H])
+	screen.set_alpha((255/layer))
+	screen = pygame.transform.smoothscale(screen, (int(WIDTH*scale/5), int(HEIGHT*scale/5)))
+	screen = pygame.transform.smoothscale(screen, (WIDTH, HEIGHT))
 	scr.blit(screen, (0,0))
 while 1:
 	timing = time()
@@ -28,5 +32,24 @@ while 1:
 				exit()
 		potok=threading.Thread(target= Layer(sc, i))
 		potok.start()
+	#pygame.display.update()
+	scale = 1 #коэ-нт разрешения цветной карты
+	color = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+	for x in range(0,WIDTH,scale):
+		for y in range(0,HEIGHT,scale):
+			col = sc.get_at((x, y))
+			col[0] = 255-col[0]
+			square = [x,y , scale,scale]
+			if col[0] <= 250 and col[0] >= 200:
+				i = rand(150, 170)
+				pygame.draw.rect(color, (i,i,i), square)
+			elif col[0] >= 100:
+				pygame.draw.rect(color, (0,col[0]//1.25, 0), square)
+			elif col[0] >= 95:
+				pygame.draw.rect(color, (col[0]*2,col[0]*2,0), square)
+			else:
+				pygame.draw.rect(color, (0,0,col[0]*2), square)
+	#color.set_alpha(128) #можно оставить, как коментарий, но можно не оставлять :)
+	sc.blit(color, (0, 0)) #можно сделать, как коментарий, тогда будет ЧБ вариант =)
 	pygame.display.update()
-	print(time()-timing)
+	print(time()-timing) #время - за которое создался мир
